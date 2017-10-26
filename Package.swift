9a65caa5e5cdeac5d36554a260be4f1f53b5b9d7
Petr Pavlik <petrpavlik@me.com>
2017-10-26 23:12:01 +0200

    migrate to vapor 2

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 let package = Package(
     name: "PusherNotificationsSwift",
     dependencies: [
-        .Package(url: "https://github.com/vapor/crypto.git", majorVersion: 1),
-        .Package(url: "https://github.com/vapor/engine.git", majorVersion: 1)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2)
     ]
 )
